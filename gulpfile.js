53d28f2f3e78aafe86a4ac0cc0be09014fbf7f04
Franz Geffke <franz@pantherx.org>
2021-03-15 16:07:52 +0100

    cleanup, fixes, added sitemap

--- gulpfile.js
+++ gulpfile.js
@@ -8,25 +8,10 @@ sass.compiler = require('node-sass');
 
 const { series } = require("gulp");
 
-function js(cb) {
-  pump(
-    [
-      gulp.src([
-        "node_modules/vue/dist/vue.min.js",
-        "node_modules/vue-carousel/dist/vue-carousel.min.js"
-      ]),
-      concat("bundle.min.js"),
-      uglify(),
-      gulp.dest("assets/js")
-    ],
-    cb
-  );
-}
-
 function css(cb) {
   return gulp.src('./sass/**/*.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest('./assets/css'));
 }
 
-exports.default = series(js, css);
+exports.default = series(css);
